15e2c7be9af6b64a8bd4c8cdc5b7531b47593263
ArtzHarvest <131970615+ArtzHarvest@users.noreply.github.com>
2023-05-29 12:16:24 +0200

    Update GithubUpdater.lua

--- Updater/GithubUpdater.lua
+++ Updater/GithubUpdater.lua
@@ -1,28 +1,19 @@
-local versionURL = "https://raw.githubusercontent.com/ArtzHarvest/AimwareLuas/main/Updater/versions.txt"
+        local local_version = "1.1"
+        ---@diagnostic disable-next-line: undefined-global
+        local local_script_name = GetScriptName()
+        local github_version_url = "https://raw.githubusercontent.com/ArtzHarvest/AimwareLuas/main/Updater/versions.txt"
+        local github_version = http.Get(github_version_url)
+        local github_source_url = "https://raw.githubusercontent.com/ArtzHarvest/AimwareLuas/main/Updater/GithubUpdater.lua"
 
-local localVersion = "1.1"
+        if local_version ~= tostring(github_version) then
+            print("Now updating " ..local_script_name)
+            file.Delete(local_script_name)
+            print("Successfully deleted old version of " ..local_script_name)
+            file.Write(local_script_name, http.Get(github_source_url))
+            local_version = github_version
+            print("Successfully updated " ..local_script_name)
+        ---@diagnostic disable-next-line: undefined-global
+            UnloadScript(local_script_name)
+        end
 
-local function downloadVersion()
-    http.Get(versionURL, function(content)
-        checkVersion(content)
-    end)
-end
-
-local function checkVersion(remoteVersion)
-    if remoteVersion > localVersion then
-        local scriptURL = "https://raw.githubusercontent.com/ArtzHarvest/AimwareLuas/main/Updater/GithubUpdater.lua"
-
-        http.Get(scriptURL, function(scriptContent)
-            updateScript(scriptContent)
-        end)
-    end
-end
-
-local function updateScript(newScript)
-    file.Write("GithubUpdater.lua", newScript)
-    LoadScript("GithubUpdater.lua")
-end
-
-downloadVersion()
-
---this is version 1.1
+        --version 1.1
